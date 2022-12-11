690ab1dd509638b1b23fd3ccc578f192dace529c
Bernardino Perea <berXpert@gmail.com>
2022-12-11 15:37:32 -0600

    Sample data

---
+++ Data/Setup.sql
@@ -0,0 +1,13 @@
+CREATE TABLE olympic_winners (
+    athlete varchar(20) DEFAULT NULL,
+    age int DEFAULT NULL,
+    country varchar(20) DEFAULT NULL,
+    country_group varchar(2) DEFAULT NULL,
+    year int DEFAULT NULL,
+    date varchar(20) DEFAULT NULL,
+    sport varchar(20) DEFAULT NULL,
+    gold int DEFAULT NULL,
+    silver int DEFAULT NULL,
+    bronze int DEFAULT NULL,
+    total int DEFAULT NULL
+)
