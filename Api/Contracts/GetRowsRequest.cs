479978309f562e1a8d85ecf31ae7bd382f7ef010
Bernardino Perea <berXpert@gmail.com>
2022-12-11 21:52:17 -0600

    Define the API

---
+++ Api/Contracts/GetRowsRequest.cs
@@ -0,0 +1,33 @@
+namespace Api.Contracts;
+
+public class GetRowsRequest
+{
+    public int StartRow { get; set; }
+    public int EndRow { get; set; }
+
+    public List<ColumnVO> RowGroupCols { get; set; }
+    public List<ColumnVO> ValueCols { get; set; }
+
+    public List<ColumnVO> PivotCols { get; set; }
+    
+    public bool PivotMode { get; set;}
+
+    public List<string> GroupKeys {get; set;}
+
+    public List<SortModel> SortModel {get; set;}
+        
+}
+
+public class ColumnVO
+{
+    public string Id { get; set; }  
+    public string DisplayName { get; set; }
+    public string Field { get; set; }
+    public string AggFunc { get; set; } 
+}
+
+public class SortModel
+{
+    public string ColId { get; set; }
+    public string Sort { get; set; }
+}
