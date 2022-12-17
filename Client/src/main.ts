db40753c507a0f0d6740758c036fcd4f83fa1837
berXpert <berXpert@gmail.com>
2022-12-17 08:35:17 -0600

    Get the base url

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -2,6 +2,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+export function getBaseUrl() {
+  return document.getElementsByTagName('base')[0].href;
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+const providers = [
+  { provide: 'BASE_URL', useFactory: getBaseUrl, deps: []}
+]
+
+platformBrowserDynamic(providers).bootstrapModule(AppModule)
   .catch(err => console.error(err));
