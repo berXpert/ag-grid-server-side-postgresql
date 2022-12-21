1a8b0c6c0ce1a5749d160d41e4925424c5ee0890
Bernardino Perea <berXpert@gmail.com>
2022-12-20 18:08:33 -0600

    Implement equivalent to JRaw

---
+++ Api/Common/RawJsonConverter.cs
@@ -0,0 +1,29 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Api.Common;
+
+/// <summary>
+/// Serializes the contents of a string value as raw JSON.  The string is validated as being an RFC 8259-compliant JSON payload
+/// </summary>
+public class RawJsonConverter : JsonConverter<string>
+{
+    protected virtual bool SkipInputValidation => false;
+
+    public override bool CanConvert(Type objectType)
+    {
+        return objectType == typeof(string);
+    }
+
+    public override string? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        using var doc = JsonDocument.ParseValue(ref reader);
+        return doc.RootElement.GetRawText();
+    }
+
+    public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+    {
+        // skipInputValidation : true will improve performance, but only do this if you are certain the value represents well-formed JSON!
+        writer.WriteRawValue(value, skipInputValidation: SkipInputValidation);
+    }
+}
