c8be4c7af592e87fbfa5824666952745f42a865e
Bernardino Perea <berXpert@gmail.com>
2023-01-22 22:33:08 -0600

    Set camel case json serialization options

--- api/Program.cs
+++ api/Program.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using QueryBuilder;
 
@@ -5,6 +8,14 @@ var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices((hostContext, serviceCollection)
         => serviceCollection.AddInfrastructure(hostContext.Configuration))
+    .ConfigureServices(services =>
+                {
+                    services.Configure<JsonSerializerOptions>(options =>
+                    {
+                        options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                        options.Converters.Add(new JsonStringEnumConverter());
+                    });
+                })
     .Build();
 
 host.Run();
